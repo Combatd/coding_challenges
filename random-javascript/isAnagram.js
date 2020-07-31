c31c2956956da618ed9b653c8e54fb6c159a65bb
Combatd <combatdstar@gmail.com>
2020-07-31 13:48:57 -0700

    Define isAnagram and areAnagrams

--- random-javascript/isAnagram.js
+++ random-javascript/isAnagram.js
@@ -4,8 +4,27 @@
 Anagram: A word or phrase formed by reordering the letters of another word or phrase, such as `add` and `dad`.
 */
 
+const isAnagram = (word1, word2) => {
+    if (word1.split('').sort().join() == word2.split('').sort().join()) {
+        return true;
+    }
+    return false;
+}
+
 /*
 Given an array of words have your function group the words which are anagrams next to each other.
 
 Example: [car, rats, arc, star] → [rats, star, arc, car]
 */
+const areAnagrams = (wordArray) => {
+    // hold each Anagram as a key
+    const anagrams = {}
+    wordArray.forEach(word => {
+        key = word.split('').sort().join(); // we have to keep rearranging by individual characters
+        if (key in anagrams) {
+            anagrams[key].push(word); // add to the end of it
+        } else {
+            anagrams[key] = [word] // will be in the array
+        }
+    });
+}
