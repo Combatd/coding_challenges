01d9792bd161f23dc068d6c51645bf59afb4d532
Combatd <combatdstar@gmail.com>
2020-07-30 20:16:32 -0700

    Create isAnagram.js

---
+++ random-javascript/isAnagram.js
@@ -0,0 +1,11 @@
+/*
+    ## 1) Given two strings, return true if they are anagrams of one another
+
+Anagram: A word or phrase formed by reordering the letters of another word or phrase, such as `add` and `dad`.
+*/
+
+/*
+Given an array of words have your function group the words which are anagrams next to each other.
+
+Example: [car, rats, arc, star] → [rats, star, arc, car]
+*/
