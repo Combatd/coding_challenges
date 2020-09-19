283021f2ea2b5d89dfa2144e562b53e785e3d719
Combatd <combatdstar@gmail.com>
2020-09-19 06:16:28 -0700

    Import loops.py

---
+++ python_tutorial/loops.py
@@ -0,0 +1,17 @@
+'''
+Task
+The provided code stub reads an integer, n, from STDIN. For all non-negative integers i < n, print i^2.
+
+Example
+
+n = 3
+
+The list of non-negative integers that are less than  is . Print the square of each number on a separate line.
+
+0
+1
+4
+'''
+
+if __name__ == '__main__':
+    n = int(input())
