5213c78dbb9269136ec52284c80ea19266a8220a
Combatd <combatdstar@gmail.com>
2020-09-19 04:28:50 -0700

    Solve arithmeticoperators.py

--- python_tutorial/arithmeticoperators.py
+++ python_tutorial/arithmeticoperators.py
@@ -7,6 +7,29 @@ The second line contains the difference of the two numbers (first - second).
 The third line contains the product of the two numbers.
 '''
 
+'''
+Whiteboarding
+
+* I can assume that both inputs, a and b, are integers.
+* Both positive and negative numbers can be handled with arithmetic operators.
+* I will use the print keyword to output each arithmetic operation.
+* str keyword converts integer to string
+
+Possible Test Case:
+a = 3
+b = 5
+
+outputs
+8
+-2
+15
+
+'''
+
 if __name__ == '__main__':
     a = int(raw_input())
     b = int(raw_input())
+
+print(str(a + b))
+print(str(a - b))
+print(str(a * b))
