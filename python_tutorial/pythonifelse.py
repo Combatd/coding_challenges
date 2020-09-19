90cd36a739dd7d29d9f64632ffea52d35c9d0131
Combatd <combatdstar@gmail.com>
2020-09-19 04:06:12 -0700

    Solve pythonifelse.py

--- python_tutorial/pythonifelse.py
+++ python_tutorial/pythonifelse.py
@@ -8,10 +8,15 @@ import sys
 Given an integer, , perform the following conditional actions:
 
 If n is odd, print Weird
-If n is even and in the inclusive range of  to , print Not Weird
-If n is even and in the inclusive range of  to , print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
 If n is even and greater than , print Not Weird
 '''
 
 if __name__ == '__main__':
     n = int(raw_input().strip())
+
+if n % 2 == 0 and n in range(2, 6) or n > 20:
+    print('Not Weird')
+else:
+    print('Weird')
