5e6736c75152e245b3080f584d33f945e1560a9f
Combatd <combatdstar@gmail.com>
2020-09-22 10:46:09 -0700

    SolvSolve printfunction.py

--- python_tutorial/printfunction.py
+++ python_tutorial/printfunction.py
@@ -17,5 +17,20 @@ Input Format
 The first line contains an integer n.
 '''
 
+'''
+Whiteboarding
+* I can assume the n argument being passed in is an integer.
+* With a for in loop, I will print each number i through range 1 through n
+    * I will use the end option to nest the i inside ""
+* Finally, I will print the n with the end option, which is not included in the for in loop.
+* Complexity Analysis:
+    * Space = O(n) Linear
+    * Time = O(n) Linear
+'''
+
 if __name__ == '__main__':
     n = int(input())
+    for i in range(1, n):
+        print(i, end="")
+        
+    print(n)
