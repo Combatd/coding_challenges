b9a0724cef320b5a20c8b76b21a9560353af0e68
Combatd <combatdstar@gmail.com>
2020-11-02 14:43:11 -0800

    Define #odd_or_even(number)

---
+++ ruby_tutorial/objectmethods.rb
@@ -0,0 +1,7 @@
+# Each object in Ruby may have methods associated with it. 
+# To demonstrate this, we want you to print whether a number is even or odd. 
+# A number has an even? method associated with it, which returns true or false based on the parity of the number.
+
+def odd_or_even(number)
+    number.even?
+end
