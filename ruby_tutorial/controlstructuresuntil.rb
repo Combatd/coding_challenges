7077c18997a692e66f527495c0c52ed9b8efa4e5
Combatd <combatdstar@gmail.com>
2020-11-08 11:53:03 -0800

    Test out until keyword for Ruby

---
+++ ruby_tutorial/controlstructuresuntil.rb
@@ -0,0 +1,16 @@
+# while not <condition>
+    # <code>
+# end
+
+# until <condition>
+    # <code>
+# end
+
+# <code> until <condition>  
+
+i = 0
+# while not 10 , runs from 0 to 9
+until i >= 10
+    puts "#{i} is the value"
+    i += 1
+end
