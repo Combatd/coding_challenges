3afd3529f9694f5930526f60febd8eb0f4d9cf98
Combatd <combatdstar@gmail.com>
2020-11-13 18:57:49 -0800

    Solve letAndConst.js

--- days-of-javascript/letAndConst.js
+++ days-of-javascript/letAndConst.js
@@ -29,13 +29,17 @@ function readLine() {
     return inputString[currentLine++];
 }
 
-function main() {
+ function main() {
+   let r = readLine()
+   
     // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
-
+    const PI = Math.PI;
     // Print the area of the circle:
-
+    let area = PI * Math.pow(r, 2)
+    console.log(area);
     // Print the perimeter of the circle:
-
+    const perimeter = 2 * PI * r;
+    console.log(perimeter);
     try {
         // Attempt to redefine the value of constant variable PI
         PI = 0;
