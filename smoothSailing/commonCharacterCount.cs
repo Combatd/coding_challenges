4d2035bb0e305d80a22c1dae6484c6f50ea893a8
Combatd <combatdstar@gmail.com>
2021-04-09 14:30:53 -0700

    Define incomplete #commonCharacterCount

--- smoothSailing/commonCharacterCount.cs
+++ smoothSailing/commonCharacterCount.cs
@@ -51,7 +51,26 @@ namespace arcade {
     public class SmoothSailing {
 
         int commonCharacterCount(string s1, string s2) {
-            
+            int amountOfCommonCharacters = 0; // accumulator
+
+            // split all the characters of both strings s1 and s2
+            string[] arr1 = s1.Split("");
+            string[] arr2 = s2.Split("");
+
+            List<string> strings1 = new List<string>(arr1);
+            List<string> strings2 = new List<string>(arr2);
+
+            // loop over the first string list to get the index of a matcher
+            for (int i = 0; i < strings1.Count; i++) {
+                int secondIndex = strings2.IndexOf(strings1[i]);
+                // if we have a match, add 1 to amountOfCommonCharacters, and remove the index so that we don't count it twice
+                if (strings2.Contains(strings1[i])) {
+                    strings2.RemoveAt(secondIndex);
+                    amountOfCommonCharacters++;
+                }
+            }
+
+            return amountOfCommonCharacters;
         }
 
     }
