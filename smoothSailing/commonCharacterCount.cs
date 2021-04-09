fbe6763af9d026e8f84c2233193eea43cc692ed8
Combatd <combatdstar@gmail.com>
2021-04-09 13:41:20 -0700

    Setup SmoothSailing#commonCharacterCount

---
+++ smoothSailing/commonCharacterCount.cs
@@ -0,0 +1,58 @@
+/*
+#commonCharacterCount
+
+Given two strings, find the number of common characters between them.
+
+For s1 = "aabcc" and s2 = "adcaa", the output should be
+commonCharacterCount(s1, s2) = 3.
+
+Strings have 3 common characters - 2 "a"s and 1 "c".
+
+
+[execution time limit] 3 seconds (cs)
+
+[input] string s1
+
+A string consisting of lowercase English letters.
+
+Guaranteed constraints:
+1 ≤ s1.length < 15.
+
+[input] string s2
+
+A string consisting of lowercase English letters.
+
+Guaranteed constraints:
+1 ≤ s2.length < 15.
+
+[output] integer
+
+*/
+
+using System;
+using System.IO;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using SimpleJson.Reflection;
+
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Threading;
+
+namespace arcade {
+
+    public class SmoothSailing {
+
+        int commonCharacterCount(string s1, string s2) {
+            
+        }
+
+    }
+}
