800deb284871b57cc1ea13e3e057c4e5699d6194
Combatd <combatdstar@gmail.com>
2021-04-10 14:57:29 -0700

    Setup SmoothSailing#isLucky

---
+++ smoothSailing/isLucky.cs
@@ -0,0 +1,63 @@
+/*
+Ticket numbers usually consist of an even number of digits. A ticket number is considered lucky if the sum of the first half of the digits is equal to the sum of the second half.
+
+Given a ticket number n, determine if it's lucky or not.
+
+Example
+
+For n = 1230, the output should be
+isLucky(n) = true;
+For n = 239017, the output should be
+isLucky(n) = false.
+Input/Output
+
+[execution time limit] 3 seconds (cs)
+
+[input] integer n
+
+A ticket number represented as a positive integer with an even number of digits.
+
+Guaranteed constraints:
+10 ≤ n < 106.
+
+[output] boolean
+
+true if n is a lucky ticket number, false otherwise.
+
+[C#] Syntax Tips
+
+// Prints help message to the console
+// Returns a string
+string helloWorld(string name) {
+    Console.Write("This prints to the console when you Run Tests");
+    return "Hello, " + name;
+}
+*/
+
+using System;
+using System.IO;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using SimpleJson.Reflection;
+
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Threading;
+
+namespace arcade {
+
+    public class SmoothSailing {
+    
+        bool isLucky(int n) {
+            
+        }
+
+    }
+}
