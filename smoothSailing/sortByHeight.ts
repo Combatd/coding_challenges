c4f77cedce162bb13c88641b1f75ad6a4cbf2d82
Combatd <combatdstar@gmail.com>
2021-05-10 22:35:06 -0700

    Solve #sortByHeight

--- smoothSailing/sortByHeight.ts
+++ smoothSailing/sortByHeight.ts
@@ -9,5 +9,19 @@ sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190].
 */
 
 function sortByHeight(a: number[]): number[] {
+    const negativeOneIndex: Array<number> = [];
+    const sortedNumberArray: Array<number> = [];
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] < 0) {
+            negativeOneIndex.push(i); // push the index of the current -1
+        } else {
+            sortedNumberArray.push(a[i]); // push the current element in the new array
+        }
+    }
 
+    sortedNumberArray.sort( (a, b) => a - b ); // sort the new array that has -1 removed
+    negativeOneIndex.forEach( (index) => {
+        sortedNumberArray.splice(index, 0, -1); // splice into the specific index of sortedNumberArray
+    })
+    return sortedNumberArray;
 }
