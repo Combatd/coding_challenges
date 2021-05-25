efac97a4c103bd44aa997a11f5b0c89d9ffd79aa
Combatd <combatdstar@gmail.com>
2021-05-24 20:47:36 -0700

    Attempt brute force reverseInParenthesis

--- smoothSailing/reverseInParentheses.ts
+++ smoothSailing/reverseInParentheses.ts
@@ -15,5 +15,24 @@ Because "foo(bar(baz))blim" becomes "foo(barzab)blim" and then "foobazrabblim"
 */
 
 function reverseInParentheses(inputString: string): string {
+    let firstPar: number = 0;
+    let secondPar: number = 0;
+    const alphabetExp = /[a-zA-Z]/g;
 
-}
+    if(!alphabetExp.test(inputString)) {
+        return '';
+    }
+
+    firstPar = inputString.indexOf('(')
+    secondPar = inputString.indexOf(')')
+
+    if(firstPar === -1 || secondPar === -1) {
+        return inputString;
+    } else if (firstPar === 0 && secondPar === inputString.length - 1) {
+        return inputString.slice(firstPar + 1, secondPar).split('').reverse().join('');
+    }
+
+
+    let stuff = inputString.slice(firstPar + 1, secondPar - 1).split('').reverse().join('');
+    return inputString.slice(0, firstPar - 1) + stuff + inputString.slice(secondPar + 1);
+ }
