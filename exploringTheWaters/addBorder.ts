e5a7eb35760168f1a7e960c9ec3e1fbc6ea957a5
Combatd <combatdstar@gmail.com>
2021-10-16 15:50:36 -0700

    Create addBorder.ts file

---
+++ exploringTheWaters/addBorder.ts
@@ -0,0 +1,21 @@
+/*
+Given a rectangular matrix of characters, add a border of asterisks(*) to it.
+
+For
+
+picture = ["abc",
+           "ded"]
+
+the output should be
+
+addBorder(picture) = ["*****",
+                      "*abc*",
+                      "*ded*",
+                      "*****"]
+
+*/
+
+
+function addBorder(picture: string[]): string[] {
+
+}
