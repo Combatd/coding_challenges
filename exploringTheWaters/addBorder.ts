b8013457a0ebe9a23ac9763e293e11ef2b07e9ed
Combatd <combatdstar@gmail.com>
2021-10-16 16:09:58 -0700

    Solve #addBorder

--- exploringTheWaters/addBorder.ts
+++ exploringTheWaters/addBorder.ts
@@ -17,5 +17,15 @@ addBorder(picture) = ["*****",
 
 
 function addBorder(picture: string[]): string[] {
-
+    const newPicture: string[] = picture.map((ele) => {
+        return '*' + ele + '*';
+    });
+    let numberOfAsterisks = picture[0].length + 2;
+    newPicture.unshift('');
+    newPicture.push('');
+    for(let i = 0; i < numberOfAsterisks; i++) {
+        newPicture[0] += '*'
+        newPicture[newPicture.length - 1] += '*';
+    }
+    return newPicture;
 }
